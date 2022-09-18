dbff009498088c5c764e7276d4e0a3cd4e9832ec
roynaldi19 <roynaldi19@gmail.com>
2022-09-18 15:20:42 +0700

    test sound

--- app/src/main/java/com/roynaldi19/bpai05_01sound/MainActivity.kt
+++ app/src/main/java/com/roynaldi19/bpai05_01sound/MainActivity.kt
@@ -3,8 +3,12 @@ package com.roynaldi19.bpai05_01sound
 import android.media.SoundPool
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.roynaldi19.bpai05_01sound.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
 
     private lateinit var sp: SoundPool
     private var soundId: Int = 0
@@ -12,6 +16,27 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        sp = SoundPool.Builder()
+            .setMaxStreams(10)
+            .build()
+
+        sp.setOnLoadCompleteListener { _, _, status ->
+            if (status == 0) {
+                spLoaded = true
+            } else {
+                Toast.makeText(this@MainActivity, "Gagal load", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        soundId = sp.load(this, R.raw.clinking_glasses, 1)
+
+        binding.btnSoundPool.setOnClickListener {
+            if (spLoaded) {
+                sp.play(soundId, 1f, 1f, 0, 0, 1f)
+            }
+        }
     }
 }
